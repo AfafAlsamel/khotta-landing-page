027a765747243119c7786c5f9c8ed0d6d8237e37
AfafYa20 <Afaf.ya27@gmail.com>
2021-11-14 20:51:14 +0300

    Navbar

---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+    { href: '/hello', title: 'Home' },
+    { href: '/hello', title: 'Downloads' },
+    { href: '/hello', title: 'Sirius' },
+    { href: '/hello', title: 'About' },
+  ]
+  
+  export default headerNavLinks
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import LayoutWrapper from '../components/LayoutWrapper'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
+  
+  return (
+    <>
+      
+    
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+ </>
+  )
+  }
 export default MyApp
